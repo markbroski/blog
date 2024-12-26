57d2d5b4533f59acac4b11536997e2c77a61286f
Mark Broski <mark@mbroski.com>
2024-12-26 17:43:17 -0500

    everything is working

---
+++ src/content.config.ts
@@ -0,0 +1,20 @@
+// 1. Import utilities from `astro:content`
+import {defineCollection, z} from 'astro:content';
+
+// 2. Import loader(s)
+import {glob, file} from 'astro/loaders';
+
+// 3. Define your collection(s)
+const blogs = defineCollection(
+    {
+        loader: glob({pattern: "**/*.md", base: './src/pages/blogs'}),
+        schema: z.object({
+            title: z.string(),
+            description: z.string(),
+            pubDate: z.coerce.date(),
+            updatedDate: z.coerce.date().optional(),
+        })
+    }
+);
+
+export const collections = {blogs};
